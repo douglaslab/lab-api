9099583f8d4807d753830347ff07b1c0f9097236
Guy Vider <guy@guyvider.com>
2015-05-28 09:34:42 -0700

    changed function name to reflect search tems

--- routes/items.js
+++ routes/items.js
@@ -3,7 +3,7 @@
 module.exports = function(server) {
   var items = require('../models/items');
   server.get('/items', items.findAll);
-  server.get('/items/:id', items.findOne);
+  server.get('/items/:id', items.findById);
   server.post('/items', items.create);
   server.put('/items/:id', items.update);
   server.del('/items/:id', items.delete);
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@
 module.exports = function(server) {
   var users = require('../models/users');
   server.get('/users', users.findAll);
-  server.get('/users/:email', users.findOne);
+  server.get('/users/:email', users.findByEmail);
   server.post('/users', users.create);
   server.put('/users/:email', users.update);
   server.del('/users/:email', users.delete);

286a3c3be287091ab3f028db5ee2b33f6d7e959b
Guy Vider <guy@guyvider.com>
2015-06-01 19:20:49 -0700

    cover all security functions

--- tests/security.js
+++ tests/security.js
@@ -2,6 +2,7 @@
 
 var debug = require('debug')('test:security');
 var should = require('should');
+var util = require('util');
 var security = require('../models/security');
 
 describe('Security tests', () => {
@@ -26,6 +27,30 @@ describe('Security tests', () => {
     let email = 'joe@schmoe.com';
     let header = security.generateAuthorizationHeader(email, pass);
     header.startsWith('Basic').should.be.true;
-    header.substr('Basic'.length + 1).should.equal(new Buffer(email + ':' + pass).toString('base64'));
+    header.substr('Basic'.length + 1).should.equal(new Buffer(util.format('%s:%s', email, pass)).toString('base64'));
+  });
+
+  let apiKey = security.generateRandomBytes(64);
+  let apiSecret = security.generateRandomBytes(64);
+  let timestamp = parseInt(Date.now() / 1000, 10);
+  let token;
+  it('should generate a token', () => {
+    token = security.generateToken(apiKey, apiSecret, timestamp);
+    token.should.not.be.empty;
+  });
+
+  it('should validate token', () => {
+    security.validateToken(token, apiKey, apiSecret, timestamp).should.be.true;
+  });
+
+  it('should correctly parse authorization header', () => {
+    let header = util.format('key=%s, token=%s, ts=%s', apiKey, token, timestamp);
+    let result = security.parseApiAuthorizationHeader(header);
+    result.should.have.property('key');
+    result.key.should.equal(apiKey);
+    result.should.have.property('token');
+    result.token.should.equal(token);
+    result.should.have.property('ts');
+    result.ts.should.equal(timestamp.toString());
   });
 });

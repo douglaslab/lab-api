6b148a44747a0931c292e03ca2f02f03bfae291b
Guy Vider <guy@guyvider.com>
2015-06-11 11:48:33 -0700

    updated server tests

--- tests/server.js
+++ tests/server.js
@@ -8,21 +8,27 @@ var helpers = require('./helpers');
 before((done) => helpers.startServer(done));
 
 describe('Basic API tests', () => {
-  it('should return 404 error', (done) => {
+  it('should return error for accessing root', (done) => {
+    request(process.env.TEST_URL)
+      .get('/')
+      .expect(404, done);
+  });
+
+  it('should return error for undefined path', (done) => {
     request(process.env.TEST_URL)
       .get('/blahblah')
       .expect(404, done);
   });
 
-  it('should return default message at root', (done) => {
+  it('should return health', (done) => {
     request(process.env.TEST_URL)
-      .get('/')
-      .expect('Content-Type', /json/)
+      .get('/health')
       .expect(200)
       .end((err, res) => {
         debug(res.body);
         should.not.exist(err);
-        res.body.should.have.property('message');
+        res.body.should.have.property('data');
+        res.body.data.should.have.property('version');
         return done();
       });
   });

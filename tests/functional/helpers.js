189b4aa936800f5441a0a6a250f8c93a0e58d3e7
Guy Vider <guy@guyvider.com>
2015-06-13 21:24:34 -0700

    added http mocking

--- tests/functional/helpers.js
+++ tests/functional/helpers.js
@@ -1,8 +1,20 @@
 'use strict';
 
 var debug = require('debug')('test:helpers');
+var httpMocks = require('node-mocks-http');
 var server = require('../../server');
 var users = require('../../models/users');
+
+var generateRandomUser = function(permissionLevel) {
+  var rand = Math.floor(Math.random() * 1000000);
+  return {
+    name: 'test' + rand,
+    email: 'test' + rand + '@example.com',
+    password: 'blahblah',
+    permissionLevel: permissionLevel,
+    school: 'UCSF'
+  };
+};
 
 //helper function to start the service for tests
 //The environment variable makes it reentrant
@@ -21,39 +33,23 @@ exports.startServer = function(done) {
 };
 
 exports.createTestUser = function(permissionLevel, callback) {
-  var rand = Math.floor(Math.random() * 1000000);
-  var newUser = {
-    name: 'test' + rand,
-    email: 'test' + rand + '@example.com',
-    password: 'blahblah',
-    permissionLevel: permissionLevel,
-    school: 'UCSF'
-  };
-  var req = {
-    body: newUser
-  };
-  var res = {
-    json: function (status, result) {
-      debug('test user %s created', result.data.email);
-      var err = (status !== 201) ? new Error(err) : null;
-      callback(err, result.data);
-    }
-  };
-  users.create(req, res, () => {});
+  var req = httpMocks.createRequest({body: generateRandomUser(permissionLevel)});
+  var res = httpMocks.createResponse();
+  users.create(req, res, () => {
+    var result = JSON.parse(res._getData());
+    var err = result.error ? new Error(result.data) : null;
+    debug(result);
+    debug('test user %s created', result.data.email);
+    callback(err, result.data);
+  });
 };
 
 exports.deleteTestUser = function(email, callback) {
-  var req = {
-    params: {
-      email: email
-    }
-  };
-  var res = {
-    json: function (status, result) {
-      debug(result.data);
-      var err = (status !== 200) ? new Error(err) : null;
-      callback(err, result.data);
-    }
-  };
-  users.delete(req, res, () => {});
+  var req = httpMocks.createRequest({params: {email: email}});
+  var res = httpMocks.createResponse();
+  users.delete(req, res, () => {
+    var result = JSON.parse(res._getData());
+      debug(result);
+      callback(null, result.data);
+  });
 };

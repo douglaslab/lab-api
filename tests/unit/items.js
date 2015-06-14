0b2faa121184bd3510677dc8832a3302f70c9d73
Guy Vider <guy@guyvider.com>
2015-06-14 14:10:29 -0700

    completed unit tests for items

--- tests/unit/items.js
+++ tests/unit/items.js
@@ -48,7 +48,7 @@ describe('Items unit tests', () => {
       res.statusCode.should.equal(400);
       result.should.have.property('error');
       result.error.should.be.true;
-      done();
+      return done();
     });
   });
 
@@ -111,67 +111,114 @@ describe('Items unit tests', () => {
     });
   });
 
-//   it('should Update the created item', (done) => {
-//     let item = {
-//       prop1: 'val1',
-//       prop2: 'val2'
-//     };
-//     request(process.env.TEST_URL)
-//       .put('/items/' + id + '/true')
-//       .set('X-API-Authorization', generateAuthorizationHeader())
-//       .send(item)
-//       .expect('Content-Type', /json/)
-//       .expect(200)
-//       .end((err, res) => {
-//         should.not.exist(err);
-//         res.body.should.have.property('error');
-//         res.body.error.should.be.false;
-//         res.body.should.have.property('data');
-//         res.body.data.should.have.property('id');
-//         res.body.data.should.have.property('properties');
-//         res.body.data.properties.prop1.should.equal(item.prop1);
-//         res.body.data.properties.prop2.should.equal(item.prop2);
-//         Object.keys(res.body.data.properties).length.should.equal(Object.keys(item).length);
-//         debug(res.body);
-//         return done();
-//       });
-//   });
+  it('should Update the properties of the created item', (done) => {
+    newItem.name = 'updated';
+    let req = httpMocks.createRequest({
+      params: {id: id},
+      body: newItem
+    });
+    let res = httpMocks.createResponse();
+    items.update(req, res, () => {
+      let result = JSON.parse(res._getData());
+      debug(result);
+      res.statusCode.should.equal(200);
+      result.should.have.property('error');
+      result.error.should.be.false;
+      result.should.have.property('data');
+      result.data.should.have.property('id');
+      result.data.should.have.property('properties');
+      result.data.properties.should.have.property('name');
+      result.data.properties.name.should.equal(newItem.name);
+      return done();
+    });
+  });
 
-//   it('should Replace all properties of the created item', (done) => {
-//     newItem.name = 'updated';
-//     request(process.env.TEST_URL)
-//       .put('/items/' + id)
-//       .set('X-API-Authorization', generateAuthorizationHeader())
-//       .send(newItem)
-//       .expect('Content-Type', /json/)
-//       .expect(200)
-//       .end((err, res) => {
-//         should.not.exist(err);
-//         res.body.should.have.property('error');
-//         res.body.error.should.be.false;
-//         res.body.should.have.property('data');
-//         res.body.data.should.have.property('id');
-//         res.body.data.should.have.property('properties');
-//         res.body.data.properties.name.should.equal(newItem.name);
-//         debug(res.body);
-//         return done();
-//       });
-//   });
+  it('should Replace all properties of the created item', (done) => {
+    let item = {
+      prop1: 'val1',
+      prop2: 'val2'
+    };
+    let req = httpMocks.createRequest({
+      params: {
+        id: id,
+        replace: true
+      },
+      body: item
+    });
+    let res = httpMocks.createResponse();
+    items.update(req, res, () => {
+      let result = JSON.parse(res._getData());
+      debug(result);
+      res.statusCode.should.equal(200);
+      result.should.have.property('error');
+      result.error.should.be.false;
+      result.should.have.property('data');
+      result.data.should.have.property('id');
+      result.data.should.have.property('properties');
+      result.data.properties.prop1.should.equal(item.prop1);
+      result.data.properties.prop2.should.equal(item.prop2);
+      Object.keys(result.data.properties).length.should.equal(Object.keys(item).length);
+      return done();
+    });
+  });
 
-//   it('should Delete the created item', (done) => {
-//     request(process.env.TEST_URL)
-//       .del('/items/' + id)
-//       .set('X-API-Authorization', generateAuthorizationHeader())
-//       .expect('Content-Type', /json/)
-//       .expect(200)
-//       .end((err, res) => {
-//         should.not.exist(err);
-//         res.body.should.have.property('error');
-//         res.body.error.should.be.false;
-//         debug(res.body);
-//         return done();
-//       });
-//   });
+  it('should fail to Update non-existing item', (done) => {
+    newItem.name = 'updated';
+    let req = httpMocks.createRequest({
+      params: {id: '123123123123'},
+      body: newItem
+    });
+    let res = httpMocks.createResponse();
+    items.update(req, res, () => {
+      let result = JSON.parse(res._getData());
+      debug(result);
+      res.statusCode.should.equal(404);
+      result.should.have.property('error');
+      result.error.should.be.true;
+      return done();
+    });
+  });
+
+  it('should fail to Update item with illegal id', (done) => {
+    newItem.name = 'updated';
+    let req = httpMocks.createRequest({
+      params: {id: 'blahblah'},
+      body: newItem
+    });
+    let res = httpMocks.createResponse();
+    items.update(req, res, () => {
+      let result = JSON.parse(res._getData());
+      debug(result);
+      res.statusCode.should.equal(400);
+      result.should.have.property('error');
+      result.error.should.be.true;
+      return done();
+    });
+  });
+
+  it('should Delete the created item', (done) => {
+    let req = httpMocks.createRequest({params: {id: id}});
+    let res = httpMocks.createResponse();
+    items.delete(req, res, () => {
+      let result = JSON.parse(res._getData());
+      res.statusCode.should.equal(200);
+      result.should.have.property('error');
+      result.error.should.be.false;
+      return done();
+    });
+  });
+
+  it('should fail to Delete item with illegal id', (done) => {
+    let req = httpMocks.createRequest({params: {id: 'blahblah'}});
+    let res = httpMocks.createResponse();
+    items.delete(req, res, () => {
+      let result = JSON.parse(res._getData());
+      res.statusCode.should.equal(400);
+      result.should.have.property('error');
+      result.error.should.be.true;
+      return done();
+    });
+  });
 });
 
 after((done) => {

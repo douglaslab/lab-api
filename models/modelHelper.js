bed6d5b689a76285799440ae8454664821b2a0d7
Guy Vider <guy@guyvider.com>
2015-06-17 14:33:33 -0700

    improve documentation

--- models/modelHelper.js
+++ models/modelHelper.js
@@ -3,9 +3,14 @@
 var debug = require('debug')('helper');
 var ObjectId = require('mongoose').Types.ObjectId;
 
+/**
+ * @class Helper
+ * @classdesc various helper functions for other models
+ */
 var Helper = function() {
   /**
    * Check if an object is empty
+   * @memberof Helper
    * @param  {Object}  obj object to test
    * @return {Boolean}
    */
@@ -15,6 +20,7 @@ var Helper = function() {
 
   /**
    * Convert id string to a Mongoose ID
+   * @memberof Helper
    * @param  {String} id id as received from user
    * @return {Object}    Mongoose ID, or null if ilegal
    */
@@ -29,6 +35,7 @@ var Helper = function() {
 
   /**
    * Return error in JSON format
+   * @memberof Helper
    * @param  {Integer} errorCode HTTP error code
    * @param  {Object} err       Error object - can be a string
    * @param  {Object} res       Response object
@@ -39,36 +46,6 @@ var Helper = function() {
     }
     debug(err);
     res.json(errorCode, {error: true, data: (typeof err === 'string') ? err : err.message});
-  };
-
-  /**
-   * Turns query string parameters into a list of Mongoose search terms<br>
-   * By default, terms are conjunctive (i.e. term1 AND term2 AND...), unless 'operator=or' is specified<br>
-   * Search will be case insensitive, unless 'ignorecase=false' is specified
-   *
-   * @param  {Object} query query string object, containing key and values
-   * @return {Object}       Mongoose search object
-   */
-  this.parseQueryParameters = function(query) {
-    var operator = '$and';
-    var ignoreCase = true;
-    var search = {};
-
-    if(query.operator) {
-      operator = '$' + query.operator;
-      delete query.operator;
-    }
-    if(query.ignorecase) {
-      ignoreCase = query.ignorecase;
-      delete query.ignorecase;
-    }
-
-    search[operator] = Object.keys(query).map(key => {
-      var obj = {};
-      obj['properties.' + key] = ignoreCase ? {'$regex': new RegExp(query[key], 'i') } : query[key];
-      return obj;
-    });
-    return search;
   };
 };
 

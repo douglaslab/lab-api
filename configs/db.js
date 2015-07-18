f33a4e701455d5d5dbdc18409c120475cef77873
Guy Vider <guy@guyvider.com>
2015-07-18 11:18:22 -0700

    Added support for empty environment variable

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@
 
 module.exports = (function() {
   let db;
-  if(process.env.NODE_ENV === 'development' && !process.env.DB_CONNECTION) {
+  if((process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) && !process.env.DB_CONNECTION) {
     db = {name: 'Local', connection: 'mongodb://localhost/dlab'};
   }
   else {
